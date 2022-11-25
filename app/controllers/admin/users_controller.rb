06cdda3c99e96fae4e8e6a8b03303f0f711db02a
bonnaban <105739184+bonnaban@users.noreply.github.com>
2022-11-25 00:17:04 +0000

    [add]

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,22 @@
 class Admin::UsersController < ApplicationController
+  def index
+    @users = User.page(params[:page]).per(5)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+       redirect_to admin_user_path(@user)
+    end
+  end
 
 private
   def user_params
