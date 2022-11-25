cef183fd835b5bc53b22dbcab0e50911eadbce07
bonnaban <105739184+bonnaban@users.noreply.github.com>
2022-11-25 01:05:21 +0000

    [add]

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -20,6 +20,6 @@ class Admin::UsersController < ApplicationController
 
 private
   def user_params
-    params.require(:user).permit(:nickname, :image, :is_active)
+    params.require(:user).permit(:nickname, :email, :profile_image, :is_active)
   end
 end
